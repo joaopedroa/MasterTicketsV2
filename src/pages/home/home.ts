384d0037562409f47d8e523c6c3c120d7f9e3d44
joaopedroa.6 <joaopedroa.6@hotmail.com>
2018-04-23 22:44:11 -0300

    'alteracao'

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,14 +15,22 @@ export class HomePage {
   items : Observable<any[]>;
   itemsTotal:Observable<any[]>;
   count:any = [];
-
+  today = new Date();
+  ticket:any;
+ ticketAtual:any;
   constructor(public navCtrl: NavController, public database:AngularFireDatabase) {
     this.items = this.database.list('aberto', ref => ref.limitToFirst(3)).valueChanges();
-    this.itemsTotal = this.database.list('aberto').valueChanges();
+    this.itemsTotal = this.database.list('aberto').snapshotChanges().map(arr => {
+      return arr.map(snap => Object.assign(snap.payload.val(), { $key: snap.key }) )
+});
     this.itemsTotal.forEach(item => {
       this.count.push(item);
       console.log(this.count);
   });
+
+
+  
+
   }
 
  
@@ -31,21 +39,23 @@ export class HomePage {
   reconhecimentoFirmaAutenticacao(){
     let tamanho1 = this.count.length - 1
     let tamanho2 = this.count[tamanho1].length -1;
-    if(this.count.length === 1){
-      this.database.list('aberto').push({ticket: 'B1',status:'aberto',number:1})
+    if(this.count[0].length === 1){
+      this.database.list('aberto').push({ticket: 'B1',status:'aberto',number:1,date: this.today })
     }else{      
-      
-      this.database.list('aberto').push(
-        {
-          
-          ticket: 'B' +  (this.count[tamanho1][tamanho2].number  + 1),
-          status:'aberto'  ,
-          number:   this.count[tamanho1][tamanho2].number  + 1
-      
-        })
+   
+      this.ticket = this.database.list('aberto').push(
+                              {
+                                
+                                ticket: 'B' +  (this.count[tamanho1][tamanho2].number  + 1),
+                                status:'aberto'  ,
+                                number:   this.count[tamanho1][tamanho2].number  + 1,
+                                date: this.today
+                            
+                              }).key;
         
     }
-   
+  
+
   }
 
 
